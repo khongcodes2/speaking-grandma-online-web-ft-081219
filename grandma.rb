783cc64c36909a50a31b6135482af00ba2ad3b9d
K Hong <albtrossfacefall@gmail.com>
2019-08-15 22:14:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,7 @@ def speak_to_grandma(string)
     "I LOVE YOU TOO PUMPKIN!"
   elsif string.upcase!=string
     "HUH?! SPEAK UP, SONNY!"
-  elsif string.upcase==string
+  else
     "NO, NOT SINCE 1938!"
   end
 end
