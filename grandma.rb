2ab2787bfbc089345c1b773931fec5ce2ba6c7ed
K Hong <albtrossfacefall@gmail.com>
2019-08-15 22:13:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,6 +12,9 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 def speak_to_grandma(string)
-  if
+  if string.upcase!=string
+    "HUH?! SPEAK UP, SONNY!"
+  elsif
+    "NO, NOT SINCE 1938!"
   end
 end
